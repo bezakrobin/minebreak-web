90908bcf626d6b5cc77aa117e87be89c25c149d2
Robin Bezak <bezakrobin@gmail.com>
2024-01-05 23:15:00 +0100

    added recent post headline and subline

--- src/components/Blog/RecentPost/RecentPost.tsx
+++ src/components/Blog/RecentPost/RecentPost.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-export const RecentPost = () => {
+export const RecentPost: React.FC = () => {
+    const containerStyle: React.CSSProperties = {
+        backgroundColor: 'transparent',
+        color: '#fff',
+        padding: '20px',
+        textAlign: 'center',
+    };
+
+    const titleStyle: React.CSSProperties = {
+        fontWeight: 'bold',
+        fontSize: '24px',
+        marginBottom: '10px',
+    };
+
+    const subtitleStyle: React.CSSProperties = {
+        fontSize: '16px',
+    };
+
     return (
-        <>RecentPost</>
+        <div style={containerStyle}>
+            <div style={titleStyle}>MineBreak News</div>
+            <div style={subtitleStyle}>Stay up to date with the latest blogs & updates!</div>
+        </div>
     );
-}
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import { Menu } from "../../components/Menu/Menu";
 import {menuItems, menuItemDiscord, menuItemServerLink, heroVideo, heroImage} from "../../data/data"
 import {Hero} from "../../components/Hero/Hero";
 import {Spacing} from "../../components/Spacing/Spacing";
+import {RecentPost} from "../../components/Blog/RecentPost/RecentPost";
 export const Home = () => {
     return (
         <>
             <Menu menuItems={menuItems} menuItemDiscord={menuItemDiscord} menuItemServerLink={menuItemServerLink} />
             <Spacing direction={'vertical'} spacing={20} />
             <Hero video={heroVideo} image={heroImage}/>
+            <RecentPost />
         </>
     );
 }
